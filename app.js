5d5855882348444c8dff405b2efddd460db15440
nikilneosoft <nikil.kurian@neosoftmail.com>
2023-10-05 17:45:49 +0530

    adding mongodb connection with mongoose

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const moduleRoute = require('./Routes/Module.route');
 
 
 const app = express();
-
 dotenv.config();
 const PORT = process.env.PORT || 3500;
+mongoose.connect(`mongodb://127.0.0.1:27017/restapi`).then(()=>{    
+
+    console.log('Mongodb connected');
+}).catch(err => console.log(err));
 
 app.use('/',moduleRoute);
 
