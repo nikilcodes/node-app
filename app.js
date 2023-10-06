da19b3d972ec6a98bb30ac94726e2a332d755abf
nikilneosoft <nikil.kurian@neosoftmail.com>
2023-10-06 13:04:02 +0530

    adding db connection first before server connection

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ const moduleRoute = require('./Routes/Module.route');
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 dotenv.config();
 const PORT = process.env.PORT || 3500;
 connection().catch(err => console.log(err));
 
 app.use('/',moduleRoute);
 
-app.listen(PORT,()=>{
-    console.log(`Listening on server ${PORT}`);
-});
+
 
 async function connection(){
     await mongoose.connect(`mongodb://127.0.0.1:27017/restapi`).then(()=>{    
             console.log('Mongodb connected');
-    });
+    }).then(()=>{
+        app.listen(PORT,()=>{
+            console.log(`Listening on server ${PORT}`);
+        });
+    })
 }
