a0f25a40c20e62edd0ce397a94ed0349ab086959
nikilneosoft <nikil.kurian@neosoftmail.com>
2023-10-06 12:40:46 +0530

    adding product model

--- Models/Product.model.js
+++ Models/Product.model.js
@@ -1,5 +1,5 @@
-import mongoose  from "mongoose";
-const { Schema } = mongoose;
+const mongoose = require("mongoose");
+const Schema  = mongoose.Schema;
 
 const ProductSchema = new Schema({
     name:{
--- Routes/Products.route.js
+++ Routes/Products.route.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const route = express.Router();
-
+const Product = require('../Models/Product.model');
 route.get('/',(req,res)=>{
     res.send('These are all products');
 });
 
 route.post('/',(req,res)=>{
+    const product = new Product({
+        name:req.body.name,
+        price:req.body.price
+    })
     res.send('Product updated');
 });
 
 route.get('/:id',(req,res)=>{
-    res.send('These products by id');
+
+    res.send(req.params);
 });
 
 route.patch('/:id',(req,res)=>{
